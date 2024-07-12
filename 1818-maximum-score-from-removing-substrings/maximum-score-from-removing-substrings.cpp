a134230fda5b98bfce820e14870a5bdfa8e46e2d
metanial <96279772+metanial@users.noreply.github.com>
2024-07-12 11:41:22 +0530

    Time: 56 ms (93.09%) | Memory: 18.3 MB (75.61%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,44 +1,35 @@
 class Solution {
 public:
-    void getCount(string str, string sub, int& cnt1, int& cnt2) {
-    
-        char first = sub[0], second = sub[1];
-        int i = 1;
-        while(i < str.length()) {
-            if(i > 0 && str[i-1] == first && str[i] == second) {
-                cnt1++;
-                str.erase(i-1, 2);
-                i--;
-                continue;
-            }
-            i++;
-        }
-
-        i = 1;
-        while(i < str.length()) {
-            if(i > 0 && str[i-1] == second && str[i] == first) {
-                cnt2++;
-                str.erase(i-1, 2);
-                i--;
-                continue;
-            }
-            i++;
-        }
-        return;
-    }
-    
     int maximumGain(string s, int x, int y) {
-        
-        int mxABcnt = 0;
-        int mxBAcnt = 0;
-        int minBAcnt = 0;
-        int minABcnt= 0;
+        // Ensure "ab" always has more points than "ba"
+        if (x < y) {
+            int temp = x;
+            x = y;
+            y = temp;
+            reverse(s.begin(), s.end());
+        }
 
-        getCount(s, "ab", mxABcnt, minBAcnt);
-        getCount(s, "ba", mxBAcnt, minABcnt);
+        int aCount = 0, bCount = 0, totalPoints = 0;
 
-        int operation1 = mxABcnt * x + minBAcnt * y;
-        int operation2 = mxBAcnt * y + minABcnt * x;
-        return max(operation1, operation2);
+        for (int i = 0; i < s.size(); ++i) {
+            char currentChar = s[i];
+
+            if (currentChar == 'a') {
+                ++aCount;
+            } else if (currentChar == 'b') {
+                if (aCount > 0) {
+                    --aCount;
+                    totalPoints += x;
+                } else {
+                    ++bCount;
+                }
+            } else {
+                totalPoints += min(bCount, aCount) * y;
+                aCount = bCount = 0;
+            }
+        }
+
+        totalPoints += min(bCount, aCount) * y;
+        return totalPoints;
     }
 };
