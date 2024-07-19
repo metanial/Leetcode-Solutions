2ffa2e3dd24108847b6b960c36b6978d13d20331
metanial <96279772+metanial@users.noreply.github.com>
2024-07-19 20:36:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (80.54%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long uniquePaths(int m, int n) {
+        long long ans = 1;
+        int totalSteps = m + n - 2;
+        int stepsToChoose = std::min(m - 1, n - 1);
+
+        // Calculate C(totalSteps, stepsToChoose) directly
+        for (int i = 1; i <= stepsToChoose; ++i) {
+            ans = ans * (totalSteps - i + 1) / i;
+        }
+        return ans;
+    }
+};
