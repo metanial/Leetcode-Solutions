4b436a665247b4eb9f762b0420e45b3a379f401c
metanial <96279772+metanial@users.noreply.github.com>
2024-08-07 17:52:08 +0530

    Time: 15 ms (88.75%) | Memory: 77.6 MB (19.51%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,29 +11,29 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* ans = new ListNode(0);
-        ListNode* temp = ans;
-        int rem = 0;
+        ListNode* result = new ListNode(0);
+        ListNode* tail = result;
+        int carry = 0;
         while(l1 && l2) {
-            temp->next = new ListNode((l1->val + l2->val + rem)%10);
-            rem = (l1->val + l2->val + rem)/10;
+            tail->next = new ListNode((l1->val + l2->val + carry)%10);
+            carry = (l1->val + l2->val + carry)/10;
             l1 = l1->next;
             l2 = l2->next;
-            temp = temp->next;
+            tail = tail->next;
         }
         while(l1) {
-            temp->next = new ListNode((l1->val + rem)%10);
-            rem = (l1->val + rem)/10;
+            tail->next = new ListNode((l1->val + carry)%10);
+            carry = (l1->val + carry)/10;
             l1 = l1->next;
-            temp = temp->next;
+            tail = tail->next;
         }
         while(l2) {
-            temp->next = new ListNode((l2->val + rem)%10);
-            rem = (l2->val + rem)/10;
+            tail->next = new ListNode((l2->val + carry)%10);
+            carry = (l2->val + carry)/10;
             l2 = l2->next;
-            temp = temp->next;
+            tail = tail->next;
         }
-        if(rem > 0) temp->next = new ListNode(rem);
-        return ans->next;
+        if(carry > 0) tail->next = new ListNode(carry);
+        return result->next;
     }
 };
