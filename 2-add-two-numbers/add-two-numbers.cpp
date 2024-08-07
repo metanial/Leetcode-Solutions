4142f0800fe0cdd22c890fa1774aeeecea483a76
metanial <96279772+metanial@users.noreply.github.com>
2024-08-07 17:33:58 +0530

    Time: 18 ms (76.17%) | Memory: 77.5 MB (19.51%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,28 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummyHead = new ListNode(0);
-        ListNode* tail = dummyHead;
-        int carry = 0;
-
-        while (l1 != nullptr || l2 != nullptr || carry != 0) {
-            int digit1 = (l1 != nullptr) ? l1->val : 0;
-            int digit2 = (l2 != nullptr) ? l2->val : 0;
-
-            int sum = digit1 + digit2 + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-
-            ListNode* newNode = new ListNode(digit);
-            tail->next = newNode;
-            tail = tail->next;
-
-            l1 = (l1 != nullptr) ? l1->next : nullptr;
-            l2 = (l2 != nullptr) ? l2->next : nullptr;
+        ListNode* ans = new ListNode(0);
+        ListNode* temp = ans;
+        int rem = 0;
+        while(l1 && l2) {
+            temp->next = new ListNode((l1->val + l2->val + rem)%10);
+            rem = (l1->val + l2->val + rem)/10;
+            l1 = l1->next;
+            l2 = l2->next;
+            temp = temp->next;
         }
-
-        ListNode* result = dummyHead->next;
-        delete dummyHead;
-        return result;
+        while(l1) {
+            temp->next = new ListNode((l1->val + rem)%10);
+            rem = (l1->val + rem)/10;
+            l1 = l1->next;
+            temp = temp->next;
+        }
+        while(l2) {
+            temp->next = new ListNode((l2->val + rem)%10);
+            rem = (l2->val + rem)/10;
+            l2 = l2->next;
+            temp = temp->next;
+        }
+        if(rem > 0) temp->next = new ListNode(rem);
+        return ans->next;
     }
 };
