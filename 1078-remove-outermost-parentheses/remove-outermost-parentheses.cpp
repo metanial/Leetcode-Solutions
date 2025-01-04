136c21fab7507e8b017e90e3cd64030d6444a061
metanial <96279772+metanial@users.noreply.github.com>
2025-01-04 21:15:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (71.68%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        int len = s.size();
-        int count = 0;
-        for(int i=0; i<len; i++) {
-            if(s[i]=='(') count++;
-            else count--;
+        int count = 0, len = s.size();
+        string sNew = "";
 
-            if(count == 1 && s[i] == '(') {
-                s.erase(s.begin()+i);
-                i--;
-                len--;
+        for(int i=0; i<len; i++) {
+            if(s[i] == '(') {
+                if(count > 0) {
+                    sNew.push_back('(');
+                }
+                count++;
             }
-            else if(count == 0 && s[i] == ')') {
-                s.erase(s.begin()+i);
-                i--;
-                len--;
+            else {
+                if(count > 1) {
+                    sNew.push_back(')');
+                }
+                count--;
             }
-            
         }
-        return s;
+        return sNew;
     }
 };
