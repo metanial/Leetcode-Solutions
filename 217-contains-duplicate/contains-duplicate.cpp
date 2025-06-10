c069bfd05220d55f7f951e9b0d9e7d22bcc1c780
metanial <96279772+metanial@users.noreply.github.com>
2025-06-10 21:02:23 +0530

    Time: 103 ms (12.24%) | Memory: 92.5 MB (18.49%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i=0; i<nums.size()-1; i++) {
-            if(nums[i]==nums[i+1]) return true;
+        int size = nums.size();
+        set<int> seen;
+
+        for(int i=0; i<size; i++) {
+            if(seen.find(nums[i]) != seen.end()) return true;
+            seen.insert(nums[i]);
         }
         return false;
     }
