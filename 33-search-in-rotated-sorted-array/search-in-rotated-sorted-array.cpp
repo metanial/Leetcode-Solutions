e4786a6fe1b98d41e336d6536b457c666910aaa1
metanial <96279772+metanial@users.noreply.github.com>
2025-08-13 22:27:51 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (65.98%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int size = nums.size();
-        int s = 0, e = size-1;
-
-        while(s <= e) {
-            int m = s + (e-s)/2;
+        int s = 0, e = nums.size()-1;
+        while(s<=e) {
+            int m = s+(e-s)/2;
             if(nums[m] == target) return m;
-
-            if(target >= nums[0] && nums[m] < nums[0]) e = m-1;
-            else if(target < nums[0] && nums[m] >= nums[0]) s = m+1;
-            else if(nums[m] > target) e = m-1;
-            else s = m+1;
+            // if left sorted
+            if(nums[s] <= nums[m]) {
+                if(nums[s] <= target && target <= nums[m]) e = m-1;
+                else s = m+1;
+            }
+            // right sorted
+            else {
+                if(nums[m] <= target && target <= nums[e]) s = m+1;
+                else e = m-1;
+            }
         }
         return -1;
     }
 };
+
+// Approach
+// Find the sorted half
